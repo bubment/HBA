56d7289f046f7fb58c2630ddd55478538e1fe90e
bubment <medgyesiattila@elektromont.hu>
2019-08-22 12:11:39 +0200

    Oldal feltöltés
    ....

---
+++ honlap-inprogress/javascripts/kozosseg-gallery-function.js
@@ -0,0 +1,168 @@
+//-------------------------------------//
+// init Colcade
+
+var $grid = $('.grid').colcade({
+  columns: '.grid__column',
+  items: '.grid__item',
+});
+
+//-------------------------------------//
+// hack CodePen to load pens as pages
+
+var nextPenSlugs = [
+  '202252c2f5f192688dada252913ccf13',
+  'a308f05af22690139e9a2bc655bfe3ee',
+  '6c9ff23039157ee37b3ab982245eef28',
+];
+
+function getPenPath() {
+  var slug = nextPenSlugs[ this.loadCount ];
+  if ( slug ) {
+    return 'https://s.codepen.io/desandro/debug/' + slug;
+  }
+}
+
+//-------------------------------------//
+// init Infinte Scroll
+
+// add items for first column
+// colcade will move items into other columns
+var $firstColumn = $grid.find('.grid__column').eq(0);
+$firstColumn.infiniteScroll({
+  path: getPenPath,
+  append: '.grid__item',
+  status: '.page-load-status',
+});
+
+// append items with colcade on append
+$firstColumn.on( 'append.infiniteScroll', function( event, response, path, items ) {
+  $grid.colcade( 'append', items );
+});
+
+
+// particles js stuff
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 88,
+      "density": {
+        "enable": true,
+        "value_area": 700
+      }
+    },
+    "color": {
+      "value": ["#aa73ff", "#f8c210", "#83d238", "#33b1f8"]
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 15
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1.5,
+        "opacity_min": 0.15,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 2.5,
+      "random": false,
+      "anim": {
+        "enable": true,
+        "speed": 2,
+        "size_min": 0.15,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 110,
+      "color": "#33b1f8",
+      "opacity": 0.25,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 1.6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": false,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function() {
+  stats.begin();
+  stats.end();
+  if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+    count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+  }
+  requestAnimationFrame(update);
+};
+requestAnimationFrame(update);;
+
+
